d38104fcbcd5c6d7ce632f6a1290f659fb3c521c
emre-unaldi <emree.unaldi@gmail.com>
2023-05-14 19:20:12 +0300

    Calculate Body Mass Index Completed

---
+++ src/CalculateBodyMassIndex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CalculateBodyMassIndex {
+    public static void main(String[] args) {
+        double kg, m, bodyMassIndex;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Boyunuzu metre cinsinden giriniz : ");
+        m = input.nextDouble();
+
+        System.out.print("Kilonuzu giriniz : ");
+        kg = input.nextDouble();
+
+        bodyMassIndex = kg / (m * m);
+
+        System.out.println("Vücut Kitle Endeksi : " + bodyMassIndex);
+    }
+}
