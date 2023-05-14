cc91508e1b9dc43f8ed33930dd037e52e71a6c35
emre-unaldi <emree.unaldi@gmail.com>
2023-05-14 17:46:38 +0300

    Calculate Hypotenuse && Calculate Triangle Area

---
+++ src/calculateTriangleArea.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class calculateTriangleArea {
+    public static void main(String[] args) {
+        int firstEdge, secondEdge, thirdEdge;
+        double perimeteorTriangle, uValue, areaTriangle;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Birinci kenar uzunluğu : ");
+        firstEdge = input.nextInt();
+
+        System.out.print("İkinci kenar uzunluğu : ");
+        secondEdge = input.nextInt();
+
+        System.out.print("Üçüncü kenar uzunluğu : ");
+        thirdEdge = input.nextInt();
+
+        uValue = (firstEdge + secondEdge + thirdEdge) / 2.00;
+        perimeteorTriangle = uValue * 2;
+        areaTriangle = Math.sqrt(uValue * (uValue - firstEdge) * (uValue - secondEdge)* (uValue - thirdEdge));
+
+        System.out.print("Üçgenin Çevresi : " + perimeteorTriangle);
+        System.out.print("\nÜçgenini Alanı : " + areaTriangle);
+    }
+}
