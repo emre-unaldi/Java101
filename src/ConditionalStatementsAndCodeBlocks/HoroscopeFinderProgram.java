4be2ffa332834d32695fc1ff941a994383661c0c
emre-unaldi <emree.unaldi@gmail.com>
2023-05-15 01:49:43 +0300

    Horoscope Finder Program Completed

---
+++ src/ConditionalStatementsAndCodeBlocks/HoroscopeFinderProgram.java
@@ -0,0 +1,160 @@
+package ConditionalStatementsAndCodeBlocks;
+import java.util.Scanner;
+public class HoroscopeFinderProgram {
+    public static void main(String[] args) {
+        int month, day;
+        boolean isError = false;
+        String horoscope = null;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Doğduğunuz ayı giriniz : ");
+        month = input.nextInt();
+        System.out.print("Doğduğunuz günü giriniz : ");
+        day = input.nextInt();
+
+        switch (month) {
+            case 1:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22 ) {
+                        horoscope = "Oğlak";
+                    } else {
+                        horoscope = "Kova";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 2:
+                if (day >= 1 && day <= 28) {
+                    if (day < 20 ) {
+                        horoscope = "Kova";
+                    } else {
+                        horoscope = "Balık";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 3:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22 ) {
+                        horoscope = "Balık";
+                    } else {
+                        horoscope = "Koç";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 4:
+                if (day >= 1 && day <= 30) {
+                    if (day < 20 ) {
+                        horoscope = "Koç";
+                    } else {
+                        horoscope = "Boğa";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 5:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22 ) {
+                        horoscope = "Boğa";
+                    } else {
+                        horoscope = "İkizler";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 6:
+                if (day >= 1 && day <= 30) {
+                    if (day < 22 ) {
+                        horoscope = "İkizler";
+                    } else {
+                        horoscope = "Yengeç";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 7:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22 ) {
+                        horoscope = "Yengeç";
+                    } else {
+                        horoscope = "Aslan";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 8:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22 ) {
+                        horoscope = "Aslan";
+                    } else {
+                        horoscope = "Başak";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 9:
+                if (day >= 1 && day <= 30) {
+                    if (day < 22 ) {
+                        horoscope = "Başak";
+                    } else {
+                        horoscope = "Terazi";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 10:
+                if (day >= 1 && day <= 31) {
+                    if (day < 22 ) {
+                        horoscope = "Terazi";
+                    } else {
+                        horoscope = "Akrep";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 11:
+                if (day >= 1 && day <= 30) {
+                    if (day < 21 ) {
+                        horoscope = "Akrep";
+                    } else {
+                        horoscope = "Yay";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            case 12:
+                if (day >= 1 && day <= 31) {
+                    if (day < 21 ) {
+                        horoscope = "Yay";
+                    } else {
+                        horoscope = "Oğlak";
+                    }
+                } else {
+                    isError = true;
+                }
+                break;
+            default:
+                isError = true;
+                break;
+        }
+
+        if (isError) {
+            System.out.println("Hatalı giriş yaptınız, tekrar deneyiniz.");
+        } else {
+            System.out.println("Burcunuz : " + horoscope);
+        }
+    }
+}
