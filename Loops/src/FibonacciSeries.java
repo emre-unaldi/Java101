02d75638298635110186a4b22a30c9896b4d0c7a
emre-unaldi <emree.unaldi@gmail.com>
2023-06-26 00:19:14 +0300

    FibonacciSeries

---
+++ Loops/src/FibonacciSeries.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int count, total = 0;
+        Scanner scanner = new Scanner(System.in);
+        int numberOne = 0;
+        int numberTwo = 1;
+
+        System.out.print("Seri:");
+        count = scanner.nextInt();
+
+        for (int i = 1; i <= count; i++) {
+            System.out.print(numberOne + " ");
+            total = numberOne + numberTwo;
+            numberOne = numberTwo;
+            numberTwo = total;
+        }
+    }
+}
