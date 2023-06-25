98b1060e2dc2f341af02dbb2d809a6f1aeb28e44
emre-unaldi <emree.unaldi@gmail.com>
2023-06-25 21:11:53 +0300

    update

--- Loops/src/FindingHarmonicNumbers.java
+++ Loops/src/FindingHarmonicNumbers.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class FindingHarmonicNumbers {
+import java.util.Scanner;
+
+public class FindingHarmonicNumbers {
+    public static void main(String[] args) {
+        int getValue;
+        double totalD = 0.0;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Sayı Giriniz : ");
+        getValue = scanner.nextInt();
+
+        for (int i = 1; i <= getValue; i++) {
+            totalD += (double) 1 / i;
+        }
+
+        System.out.println("Harmonik Sayı : " + totalD);
+    }
 }
